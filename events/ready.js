12ec55b451e0bf913a881e45042b89130947b7e1
Gabor Demeter <gabordemeter29@gmail.com>
2021-02-22 15:40:14 +0100

    ready stability

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,15 @@ module.exports = async() => {
     console.log("ready")
     client.user.setActivity(`${client.config.prefix}help`, {type: "WATCHING"});
 
+    for await(guild of client.guilds.cache){
+      let guildID=guild[0]
+      const rows=await db.query(`SELECT id FROM guild WHERE id=${guildID}`).then(rows=>{return rows});
+
+      if(rows.length===0){   
+        await db.query(`INSERT INTO guild VALUES('${guildID}', '${client.config.defaultPrefix}', 0)`); 
+      }
+    }
+    
     const guilds=await db.query(`SELECT id,prefix FROM guild`).then(rows =>{ return rows});
     
     client.guildPrefixes= new Map();  
@@ -12,6 +21,5 @@ module.exports = async() => {
       client.guildPrefixes.set(guild["id"], guild["prefix"]);
 
     }
-
     
   };
