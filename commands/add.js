ac27b2d3f2530788698b80eea331df77b3d84610
Gabor Demeter <gabordemeter29@gmail.com>
2021-07-24 16:43:24 +0200

    added ratelimit for adding channels

--- commands/add.js
+++ commands/add.js
@@ -4,9 +4,9 @@ const client=require("../constants/client.js")
 const asleep=require("asleep")
 const setupFilter=require("../filters/setupFilter.js");
 const fetchMessages = require("../scripts/fetchMessages.js");
-//client.channelAddings= new Map();  implement later? But maybe we dont need to, depends how much traffic will the bot have.
-
-exports.run = async(message, args) =>{
+const amountOfRecentlyAddedChannels= new Map();
+                            
+exports.run = async (message, args) => {
     const prefix= client.guildPrefixes.get(message.guild.id);
     if(args.length!=1 || !message.mentions.channels.first() || message.mentions.channels.first().type!="text"){
         return client.commands.get("help").run(message, ["add"]);
@@ -15,20 +15,49 @@ exports.run = async(message, args) =>{
     const mentionedChannel=message.mentions.channels.first();
     if(!mentionedChannel.viewable) return message.channel.send("I can't access the channel");
 
-    db.query(`SELECT id FROM channel WHERE id='${mentionedChannel.id}'`)
-        .then(rows=>{
-            if(rows.length>0){
-                const alreadyAddedEmbed = new Discord.MessageEmbed()
-                .setTitle("Error: This Channel is already added")
-                .setDescription(`To check the already added channels, use the \`${prefix}guild\` command.`)
+    let rows = await db.query(`SELECT id FROM channel WHERE id='${mentionedChannel.id}'`).then(rows=>{return rows});
+    
+    if(rows.length>0){
+        const alreadyAddedEmbed = new Discord.MessageEmbed()
+        .setTitle("Error: This Channel is already added")
+        .setDescription(`To check the already added channels, use the \`${prefix}guild\` command.`)
+        .setColor("#ff0830")
+
+        return message.channel.send(alreadyAddedEmbed);
+    }
+    else{
+        for (fetching of client.fetchingQueue){
+            if(mentionedChannel.id==fetching.channel.id){
+                const alreadyFetchingEmbed = new Discord.MessageEmbed()
+                .setTitle("Error: This Channel is already being added")
+                .setDescription(`This channel is already in the queue, please wait.`)
                 .setColor("#ff0830")
 
-                return message.channel.send(alreadyAddedEmbed)
+                return message.channel.send(alreadyFetchingEmbed);
             }
-        });
-   
-        
-    
+        }
+    }
+           
+    if(amountOfRecentlyAddedChannels.get(mentionedChannel.guild.id)>=3){
+        const tooMuchAddingEmbed = new Discord.MessageEmbed()
+            .setTitle("Error: You are adding channels too fast!")
+            .setDescription(`Please wait a few minutes before adding more.`)
+            .setColor("#ff0830")
+
+            return message.channel.send(tooMuchAddingEmbed);
+    }
+
+    if(amountOfRecentlyAddedChannels.has(mentionedChannel.guild.id)){
+        let amount = amountOfRecentlyAddedChannels.get(mentionedChannel.guild.id);
+        amountOfRecentlyAddedChannels.set(mentionedChannel.guild.id, amount+1);
+    }
+    else{
+        amountOfRecentlyAddedChannels.set(mentionedChannel.guild.id, 1); 
+    }
+    setTimeout(function(){
+        let amount = amountOfRecentlyAddedChannels.get(mentionedChannel.guild.id); 
+        amountOfRecentlyAddedChannels.set(mentionedChannel.guild.id, amount-1); 
+    }, 900000); //15 minutes
 
     const addEmbed = new Discord.MessageEmbed()
     .setTitle(`#${mentionedChannel.name} will be added shortly`)
@@ -55,7 +84,6 @@ exports.run = async(message, args) =>{
 
     async function addMessages(channel){
         let messages = [];
-        let lastID;
         
         fetchMessages(mentionedChannel, 500).then(async result =>{
             messages = result.messages;
@@ -68,8 +96,8 @@ exports.run = async(message, args) =>{
             }
             await db.query(`INSERT INTO channel VALUES('${channel.id}','${channel.guild.id}')`)
                
-        }).catch( () =>{
-            //
+        }).catch( (err) =>{
+            console.log("ADD ERROR: "+err)
         }).finally( async () =>{
             client.fetchingQueue.splice(0, 1)           
             if(client.fetchingQueue[0]) {
